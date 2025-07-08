a20050e8d7f925d4de5abb20bde4cc429e4776d6
IABlock <iablocktec@gmail.com>
2025-07-08 15:53:39 -0300

    transformando em flask

--- lergmail.py
+++ lergmail.py
@@ -1,12 +1,26 @@
+from flask import Flask, jsonify, make_response, json
+
 from gmailleitura import lerEmail
 from gmailauth import autenticacao
 from emailLink import listaLinks
 
-if __name__ == "__main__":
+app = Flask(__name__)
+
+@app.route("/")
+def listalinks():
   credenciais = autenticacao()
   if credenciais is not None:
     htmlEmail = lerEmail(credenciais)
     linkEncontrados = listaLinks(htmlEmail)
-    print(linkEncontrados)
+
+    lista = json.dumps(linkEncontrados)        
+    response = make_response(lista,200)
+    response.headers["Content-Type"]="application/json"
+
+    return response
   else:
     print('Credenciais Invalidas')
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
