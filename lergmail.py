3649cf8b55c5f2b880d4bfbfdfc2033e277b6cad
IABlock <iablocktec@gmail.com>
2025-07-07 21:45:38 -0300

    lendo email especifico

--- lergmail.py
+++ lergmail.py
@@ -46,17 +46,19 @@ def main():
     for label in labels:
       print(label["name"])
 
-    # Tentar ler e-mails
+    # Lista Geral de um remitente especifico
     # https://www.googleapis.com/gmail/v1/users/me/messages?q=in:sent after:2014/01/01 before:2014/02/01
     servico = build("gmail","v1",credentials=creds,)
     resultado = servico.users().messages().list(userId="me", maxResults=500, q='from:no-reply@99freelas.com.br').execute()
     ids=[]
-    print(resultado)
     for message in resultado['messages']:      
       ids.append(message['id'])
 
-    idmessage = '197c2aa7c5610e07'
-    #servico = build
+    #print(ids)
+    idmessage = '197e454e6a078944'
+    for idmessage in ids:
+      resultado = servico.users().messages().get(userId="me", id=idmessage).execute()
+      print(resultado)
 
   except HttpError as error:
     # TODO(developer) - Handle errors from gmail API.
