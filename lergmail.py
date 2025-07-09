d130e544159abe0f8e50ac4d62e5f1862e0424ad
IABlock <iablocktec@gmail.com>
2025-07-09 16:01:09 -0300

    grava linha notion

--- lergmail.py
+++ lergmail.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, make_response, json
 from gmailleitura import lerEmail
 from gmailauth import autenticacao
 from emailLink import listaLinks
-from notionDatabase import listDatabase
+from notionDatabase import listDatabase,gravaDatabase
 
 app = Flask(__name__)
 
@@ -25,6 +25,12 @@ def listalinks():
 @app.route("/notion")
 def notionlink():
   return listDatabase()
+
+@app.route("/gravanotion")
+def notionrec():
+  retorno = gravaDatabase()
+  response = make_response(retorno, 200)
+  return (response)
 
 if __name__ == "__main__":
   app.run(debug=True)
--- notionDatabase.py
+++ notionDatabase.py
@@ -3,6 +3,15 @@ from notion_client import Client
 from dotenv import load_dotenv
 from flask import jsonify, make_response
 
+def connectNotion():
+    load_dotenv()
+
+    NOTION_TOKEN = os.getenv('NOTION_TOKEN')  # Substitua pela sua chave
+    DATABASE_ID = os.getenv('NOTION_DATABASE_ID') # Substitua pelo ID da sua tabela
+
+    notion = Client(auth=NOTION_TOKEN)
+    return notion, DATABASE_ID    
+
 def get_notion_table_data(database_id,notionObj):
     try:
         results = notionObj.databases.query(database_id=database_id)["results"]
@@ -12,12 +21,8 @@ def get_notion_table_data(database_id,notionObj):
         return None
 
 def listDatabase():
-    load_dotenv()
 
-    NOTION_TOKEN = os.getenv('NOTION_TOKEN')  # Substitua pela sua chave
-    DATABASE_ID = os.getenv('NOTION_DATABASE_ID') # Substitua pelo ID da sua tabela
-
-    notion = Client(auth=NOTION_TOKEN)
+    notion,DATABASE_ID = connectNotion()
 
     data = get_notion_table_data(DATABASE_ID,notion)
 
@@ -35,3 +40,29 @@ def listDatabase():
 
         return response
 
+
+
+def criar_linha_notion(notion,database_id, nome_da_pagina, propriedades):
+    try:
+        notion.pages.create(
+            parent={"database_id": database_id},
+            properties={
+                "Name": {"title": [{"text": {"content": nome_da_pagina}}]},
+                **propriedades,
+            },
+        )
+        return {'status':'Ok'}
+    except Exception as e:
+        return {'status': str(e)}
+
+def gravaDatabase():
+
+    notion,DATABASE_ID = connectNotion()
+    
+    nome_da_nova_pagina = "Minha Nova Página"
+    propriedades_da_pagina = {
+        "Status": {"select": {"name": "Fazer"}},
+        "Data": {"date": {"start": "2024-07-10", "end": None}},
+    }
+
+    return criar_linha_notion(notion, DATABASE_ID, nome_da_nova_pagina, propriedades_da_pagina)
